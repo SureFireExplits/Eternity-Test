9ac56b24d2145c2770195181cc5bfd34adc3be74
SureFireExplits <112823358+SureFireExplits@users.noreply.github.com>
2023-03-04 04:51:33 -0600

    Add files via upload

---
+++ Eternity Text/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Eternity_Text
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.TopBar = new System.Windows.Forms.Panel();
+			this.visualStudioTabControl1 = new VisualStudioTabControl.VisualStudioTabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.siticoneButton1 = new Siticone.UI.WinForms.SiticoneButton();
+			this.siticoneButton2 = new Siticone.UI.WinForms.SiticoneButton();
+			this.siticoneButton3 = new Siticone.UI.WinForms.SiticoneButton();
+			this.siticoneButton4 = new Siticone.UI.WinForms.SiticoneButton();
+			this.siticoneButton5 = new Siticone.UI.WinForms.SiticoneButton();
+			this.siticoneButton6 = new Siticone.UI.WinForms.SiticoneButton();
+			this.siticoneButton7 = new Siticone.UI.WinForms.SiticoneButton();
+			this.label1 = new System.Windows.Forms.Label();
+			this.TextEditor = new System.Windows.Forms.WebBrowser();
+			this.siticoneButton8 = new Siticone.UI.WinForms.SiticoneButton();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.siticoneElipse1 = new Siticone.UI.WinForms.SiticoneElipse(this.components);
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.TopBar.SuspendLayout();
+			this.visualStudioTabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// TopBar
+			// 
+			this.TopBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.TopBar.Controls.Add(this.label1);
+			this.TopBar.Controls.Add(this.siticoneButton7);
+			this.TopBar.Controls.Add(this.siticoneButton6);
+			this.TopBar.Dock = System.Windows.Forms.DockStyle.Top;
+			this.TopBar.Location = new System.Drawing.Point(0, 0);
+			this.TopBar.Name = "TopBar";
+			this.TopBar.Size = new System.Drawing.Size(678, 36);
+			this.TopBar.TabIndex = 0;
+			this.TopBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+			this.TopBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+			// 
+			// visualStudioTabControl1
+			// 
+			this.visualStudioTabControl1.ActiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.visualStudioTabControl1.AllowDrop = true;
+			this.visualStudioTabControl1.BackTabColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.visualStudioTabControl1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.visualStudioTabControl1.ClosingButtonColor = System.Drawing.Color.WhiteSmoke;
+			this.visualStudioTabControl1.ClosingMessage = null;
+			this.visualStudioTabControl1.Controls.Add(this.tabPage1);
+			this.visualStudioTabControl1.HeaderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.visualStudioTabControl1.HorizontalLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.visualStudioTabControl1.ItemSize = new System.Drawing.Size(240, 16);
+			this.visualStudioTabControl1.Location = new System.Drawing.Point(4, 42);
+			this.visualStudioTabControl1.Name = "visualStudioTabControl1";
+			this.visualStudioTabControl1.SelectedIndex = 0;
+			this.visualStudioTabControl1.SelectedTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.visualStudioTabControl1.ShowClosingButton = true;
+			this.visualStudioTabControl1.ShowClosingMessage = false;
+			this.visualStudioTabControl1.Size = new System.Drawing.Size(547, 260);
+			this.visualStudioTabControl1.TabIndex = 1;
+			this.visualStudioTabControl1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.tabPage1.Controls.Add(this.TextEditor);
+			this.tabPage1.Location = new System.Drawing.Point(4, 20);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(539, 236);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Remove this tab";
+			// 
+			// listBox1
+			// 
+			this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.listBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listBox1.ForeColor = System.Drawing.Color.White;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 20;
+			this.listBox1.Location = new System.Drawing.Point(553, 42);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(120, 260);
+			this.listBox1.TabIndex = 2;
+			// 
+			// siticoneButton1
+			// 
+			this.siticoneButton1.CheckedState.Parent = this.siticoneButton1;
+			this.siticoneButton1.CustomImages.Parent = this.siticoneButton1;
+			this.siticoneButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.siticoneButton1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+			this.siticoneButton1.HoveredState.Parent = this.siticoneButton1;
+			this.siticoneButton1.Location = new System.Drawing.Point(4, 304);
+			this.siticoneButton1.Name = "siticoneButton1";
+			this.siticoneButton1.ShadowDecoration.Parent = this.siticoneButton1;
+			this.siticoneButton1.Size = new System.Drawing.Size(106, 30);
+			this.siticoneButton1.TabIndex = 3;
+			this.siticoneButton1.Text = "Execute";
+			this.siticoneButton1.Click += new System.EventHandler(this.Execute);
+			// 
+			// siticoneButton2
+			// 
+			this.siticoneButton2.CheckedState.Parent = this.siticoneButton2;
+			this.siticoneButton2.CustomImages.Parent = this.siticoneButton2;
+			this.siticoneButton2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.siticoneButton2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.siticoneButton2.ForeColor = System.Drawing.Color.White;
+			this.siticoneButton2.HoveredState.Parent = this.siticoneButton2;
+			this.siticoneButton2.Location = new System.Drawing.Point(116, 304);
+			this.siticoneButton2.Name = "siticoneButton2";
+			this.siticoneButton2.ShadowDecoration.Parent = this.siticoneButton2;
+			this.siticoneButton2.Size = new System.Drawing.Size(106, 30);
+			this.siticoneButton2.TabIndex = 4;
+			this.siticoneButton2.Text = "Clear";
+			// 
+			// siticoneButton3
+			// 
+			this.siticoneButton3.CheckedState.Parent = this.siticoneButton3;
+			this.siticoneButton3.CustomImages.Parent = this.siticoneButton3;
+			this.siticoneButton3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.siticoneButton3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.siticoneButton3.ForeColor = System.Drawing.Color.White;
+			this.siticoneButton3.HoveredState.Parent = this.siticoneButton3;
+			this.siticoneButton3.Location = new System.Drawing.Point(228, 304);
+			this.siticoneButton3.Name = "siticoneButton3";
+			this.siticoneButton3.ShadowDecoration.Parent = this.siticoneButton3;
+			this.siticoneButton3.Size = new System.Drawing.Size(106, 30);
+			this.siticoneButton3.TabIndex = 5;
+			this.siticoneButton3.Text = "Open File";
+			// 
+			// siticoneButton4
+			// 
+			this.siticoneButton4.CheckedState.Parent = this.siticoneButton4;
+			this.siticoneButton4.CustomImages.Parent = this.siticoneButton4;
+			this.siticoneButton4.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.siticoneButton4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.siticoneButton4.ForeColor = System.Drawing.Color.White;
+			this.siticoneButton4.HoveredState.Parent = this.siticoneButton4;
+			this.siticoneButton4.Location = new System.Drawing.Point(340, 304);
+			this.siticoneButton4.Name = "siticoneButton4";
+			this.siticoneButton4.ShadowDecoration.Parent = this.siticoneButton4;
+			this.siticoneButton4.Size = new System.Drawing.Size(106, 30);
+			this.siticoneButton4.TabIndex = 6;
+			this.siticoneButton4.Text = "Save File";
+			// 
+			// siticoneButton5
+			// 
+			this.siticoneButton5.CheckedState.Parent = this.siticoneButton5;
+			this.siticoneButton5.CustomImages.Parent = this.siticoneButton5;
+			this.siticoneButton5.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.siticoneButton5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.siticoneButton5.ForeColor = System.Drawing.Color.White;
+			this.siticoneButton5.HoveredState.Parent = this.siticoneButton5;
+			this.siticoneButton5.Location = new System.Drawing.Point(567, 304);
+			this.siticoneButton5.Name = "siticoneButton5";
+			this.siticoneButton5.ShadowDecoration.Parent = this.siticoneButton5;
+			this.siticoneButton5.Size = new System.Drawing.Size(106, 30);
+			this.siticoneButton5.TabIndex = 7;
+			this.siticoneButton5.Text = "Attach";
+			this.siticoneButton5.Click += new System.EventHandler(this.Attach);
+			// 
+			// siticoneButton6
+			// 
+			this.siticoneButton6.CheckedState.Parent = this.siticoneButton6;
+			this.siticoneButton6.CustomImages.Parent = this.siticoneButton6;
+			this.siticoneButton6.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.siticoneButton6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.siticoneButton6.ForeColor = System.Drawing.Color.White;
+			this.siticoneButton6.HoveredState.Parent = this.siticoneButton6;
+			this.siticoneButton6.Location = new System.Drawing.Point(638, 2);
+			this.siticoneButton6.Name = "siticoneButton6";
+			this.siticoneButton6.ShadowDecoration.Parent = this.siticoneButton6;
+			this.siticoneButton6.Size = new System.Drawing.Size(35, 30);
+			this.siticoneButton6.TabIndex = 8;
+			this.siticoneButton6.Text = "X";
+			this.siticoneButton6.Click += new System.EventHandler(this.Close);
+			// 
+			// siticoneButton7
+			// 
+			this.siticoneButton7.CheckedState.Parent = this.siticoneButton7;
+			this.siticoneButton7.CustomImages.Parent = this.siticoneButton7;
+			this.siticoneButton7.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.siticoneButton7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.siticoneButton7.ForeColor = System.Drawing.Color.White;
+			this.siticoneButton7.HoveredState.Parent = this.siticoneButton7;
+			this.siticoneButton7.Location = new System.Drawing.Point(597, 2);
+			this.siticoneButton7.Name = "siticoneButton7";
+			this.siticoneButton7.ShadowDecoration.Parent = this.siticoneButton7;
+			this.siticoneButton7.Size = new System.Drawing.Size(35, 30);
+			this.siticoneButton7.TabIndex = 9;
+			this.siticoneButton7.Text = "-";
+			this.siticoneButton7.Click += new System.EventHandler(this.Minimize);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(3, 6);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(93, 21);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "Eternity Test";
+			// 
+			// TextEditor
+			// 
+			this.TextEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.TextEditor.Location = new System.Drawing.Point(3, 3);
+			this.TextEditor.MinimumSize = new System.Drawing.Size(20, 20);
+			this.TextEditor.Name = "TextEditor";
+			this.TextEditor.Size = new System.Drawing.Size(533, 230);
+			this.TextEditor.TabIndex = 0;
+			// 
+			// siticoneButton8
+			// 
+			this.siticoneButton8.CheckedState.Parent = this.siticoneButton8;
+			this.siticoneButton8.CustomImages.Parent = this.siticoneButton8;
+			this.siticoneButton8.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.siticoneButton8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.siticoneButton8.ForeColor = System.Drawing.Color.White;
+			this.siticoneButton8.HoveredState.Parent = this.siticoneButton8;
+			this.siticoneButton8.Location = new System.Drawing.Point(526, 304);
+			this.siticoneButton8.Name = "siticoneButton8";
+			this.siticoneButton8.ShadowDecoration.Parent = this.siticoneButton8;
+			this.siticoneButton8.Size = new System.Drawing.Size(35, 30);
+			this.siticoneButton8.TabIndex = 11;
+			this.siticoneButton8.Text = "+";
+			this.siticoneButton8.Click += new System.EventHandler(this.AddTab);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.panel1.Location = new System.Drawing.Point(452, 306);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(28, 26);
+			this.panel1.TabIndex = 11;
+			// 
+			// siticoneElipse1
+			// 
+			this.siticoneElipse1.BorderRadius = 20;
+			this.siticoneElipse1.TargetControl = this.panel1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+			this.ClientSize = new System.Drawing.Size(678, 337);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.siticoneButton8);
+			this.Controls.Add(this.siticoneButton5);
+			this.Controls.Add(this.siticoneButton4);
+			this.Controls.Add(this.siticoneButton3);
+			this.Controls.Add(this.siticoneButton2);
+			this.Controls.Add(this.siticoneButton1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.visualStudioTabControl1);
+			this.Controls.Add(this.TopBar);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.Text = "Eternity Test";
+			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+			this.TopBar.ResumeLayout(false);
+			this.TopBar.PerformLayout();
+			this.visualStudioTabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel TopBar;
+		private VisualStudioTabControl.VisualStudioTabControl visualStudioTabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Label label1;
+		private Siticone.UI.WinForms.SiticoneButton siticoneButton7;
+		private Siticone.UI.WinForms.SiticoneButton siticoneButton6;
+		private System.Windows.Forms.WebBrowser TextEditor;
+		private Siticone.UI.WinForms.SiticoneButton siticoneButton1;
+		private Siticone.UI.WinForms.SiticoneButton siticoneButton2;
+		private Siticone.UI.WinForms.SiticoneButton siticoneButton3;
+		private Siticone.UI.WinForms.SiticoneButton siticoneButton4;
+		private Siticone.UI.WinForms.SiticoneButton siticoneButton5;
+		private Siticone.UI.WinForms.SiticoneButton siticoneButton8;
+		private System.Windows.Forms.Panel panel1;
+		private Siticone.UI.WinForms.SiticoneElipse siticoneElipse1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ Eternity Text/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using WeAreDevs_API;
+
+namespace Eternity_Text
+{
+	public partial class Form1 : Form
+	{
+		ExploitAPI Eternity = new ExploitAPI();
+		Point lastPoint;
+		public Form1()
+		{
+			InitializeComponent();
+
+			TextEditor.Navigate(new Uri(string.Format("file:///{0}/ace/index.html", Directory.GetCurrentDirectory())));
+		}
+
+		private void AddTab(object sender, EventArgs e)
+		{
+			TabPage tabPage = new TabPage();
+			{
+				tabPage.Name = "Script";
+				tabPage.Text = "Script " + visualStudioTabControl1.TabCount;
+				tabPage.Parent = visualStudioTabControl1;
+				TextEditor.Dock = DockStyle.Fill;
+				TextEditor.Navigate(new Uri(string.Format("file:///{0}/ace/index.html", Directory.GetCurrentDirectory())));
+				TextEditor.Parent = tabPage;
+				TextEditor.Name = "TextEditor";
+				TextEditor = new WebBrowser();
+				visualStudioTabControl1.SelectTab(tabPage);
+			}
+		}
+
+		private void Close(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void Minimize(object sender, EventArgs e)
+		{
+			WindowState = FormWindowState.Minimized;
+		}
+
+		private void Attach(object sender, EventArgs e)
+		{
+			Eternity.LaunchExploit();
+		}
+
+		private void Execute(object sender, EventArgs e)
+		{
+			try
+			{
+				if (visualStudioTabControl1.TabPages.Count > 0)
+				{
+					TabControl.TabPageCollection pages = visualStudioTabControl1.TabPages;
+
+					for (int i = 0; i < pages.Count; i++)
+					{
+						WebBrowser TextEditor = pages[i].Controls.Find("TextEditor", true).FirstOrDefault<Control>() as WebBrowser;
+						HtmlDocument document = TextEditor.Document;
+						string scriptName = "GetText";
+						object[] args = new string[0];
+						object obj = document.InvokeScript(scriptName, args);
+						string script = obj.ToString();
+
+						Eternity.SendLuaScript(TextEditor.DocumentText);
+					}
+				}
+			}
+			catch { }
+		}
+
+		private void Form1_MouseDown(object sender, MouseEventArgs e)
+		{
+			lastPoint = new Point(e.X, e.Y);
+		}
+
+		private void Form1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				this.Left += e.X - lastPoint.X;
+				this.Top += e.Y - lastPoint.Y;
+			}
+		}
+	}
+}
